7cf07a77e8401cf25ab32a0c895dd9186a1ac37d
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2024-04-09 11:29:22 -0500

    Initial Commit

---
+++ Pick-A-Pal/Pick_A_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_A_PalApp.swift
+//  Pick-A-Pal
+//
+//  Created by Justin Hold on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Pick_A_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
